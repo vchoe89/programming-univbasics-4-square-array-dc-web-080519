8958ea9390aa161d18188263510197c06d278a38
Victor Jian Choe <vchoe89@gmail.com>
2019-07-23 23:43:23 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
-  # your code here
+  counter = 0
+  new_array = []
+  while array[counter]do
+    new_array << array[counter] ** 2
+  counter += 1
+  end
+new_array
 end
+
+
+
+  
