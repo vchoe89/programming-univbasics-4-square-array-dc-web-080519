35c5178b8d29ba169e09f2d18098c6ff47c39e34
Victor Jian Choe <vchoe89@gmail.com>
2019-07-23 22:51:50 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,10 @@
 def square_array(array)
   counter = 0
-  array = [1, 2 ,3]
+  array = []
   while array[counter] do
     array.each do |n|
     new number = n**2
+    new_number << array
     return array
   end
 
